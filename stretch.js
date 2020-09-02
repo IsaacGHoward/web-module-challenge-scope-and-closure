c39be30f961a3c85c55c4cb9917965124ae3788a
IsaacGHoward <howardisaac100@gmail.com>
2020-09-02 12:23:24 -0700

    Answered Stretch

---
+++ stretch.js
@@ -0,0 +1,18 @@
+/*
+3. Research the differences between functional programming and object oriented programming. 
+Then, describe the pros and cons of functional programming vs object-oriented programming. 
+This is a common interview question and great practice!
+*/
+
+/*
+
+    Functional programming is more concerned with the data itself, while object oriented programming is more about
+    objects and classes. Functional is nice because it can be more effient, and shorter code. Because you don't have
+    to define objects and classes you just keep all the important code. However, object oriented is also nice because
+    it allows for good intereactions between the data you have, with all the structure that objects / classes give.
+    Functional is good when you have a certain amount of data, and you want to add more and more operations on that data.
+    On the other hand Object Oriented is good when you have a fixed amount of these operations, and want to add new
+    data, because of the structure in place. Both are not mutually exclusive, as you can mix concepts and have functions
+    objects and classes all coexisting if you want. But it's good to choose an overall design approach for your code.
+
+*/
